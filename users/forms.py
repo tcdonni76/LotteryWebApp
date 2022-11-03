a2de62aadd1804aa310b8811fe65583fcba738b6
tcd76 <c0088537@newcastle.ac.uk>
2022-11-03 15:04:44 +0000

    Datarequired and Email validators added to registration form

--- users/forms.py
+++ users/forms.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email
 
 
 class RegisterForm(FlaskForm):
-    email = StringField()
-    firstname = StringField()
-    lastname = StringField()
-    phone = StringField()
-    password = PasswordField()
-    confirm_password = PasswordField()
-    submit = SubmitField()
+    email = StringField(validators=[DataRequired(), Email()])
+    firstname = StringField(validators=[DataRequired()])
+    lastname = StringField(validators=[DataRequired()])
+    phone = StringField(validators=[DataRequired()])
+    password = PasswordField(validators=[DataRequired()])
+    confirm_password = PasswordField(validators=[DataRequired()])
+    submit = SubmitField(validators=[DataRequired()])
